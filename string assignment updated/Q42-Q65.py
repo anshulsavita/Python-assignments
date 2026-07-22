8ec7bcd5e9f7a6b0264c859f2bcc2a8fc7a49ef5
Anshul savita <anshulsavita370@gmail.com>
2026-07-22 19:00:49 +0530

    fix an error

--- string assignment updated/Q42-Q65.py
+++ string assignment updated/Q42-Q65.py
@@ -22,7 +22,7 @@
 # The area of the rectangle is 1256.66cm2
 # The volume of the cylinder is 1254.725cm3
 
-print("1256.66cm"chr(178))
+# print("1256.66cm"chr(178))
 
 # ********Question 44 ***********
 # Write a Python program to print the index of the character in a string.  
