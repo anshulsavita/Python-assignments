4728a1d9eb4743a7437d2cdb8b15c83824f7627a
Anshul savita <anshulsavita370@gmail.com>
2026-08-26 15:58:03 +0530

    added set assignment and change name of dict assignment file

---
+++ set assignment/Q1-Q15.py
@@ -0,0 +1,97 @@
+# *************Question 1 *************
+# Write a Python program to create a set.
+# s={1,2,2,3,4,5,5,6}
+# print(s)
+
+# ************Question 2 *************
+# Write a Python program to iteration over sets.
+# s = {10, 20, 30, 40}
+# for i in s:
+#     print(i)
+
+# *************Question 3 ************
+# Write a Python program to add member(s) in a set.
+# s={42,35,54,64,75,75}
+# print(s)
+# s.add(100)
+# print(s)
+
+# ************Question 4 **********
+# Write a Python program to remove item(s) from set
+# s={42,35,54,64,75,75}
+# s.discard(35)
+# print(s)
+
+# ************Question 5 *********
+# Write a Python program to remove an item from a set if it is present in the set.
+# s={42,35,54,64,75,75}
+# s.discard(35)
+# print(s)
+
+# ***********Question 6 **********
+# Write a Python program to create an intersection of sets. 
+# s1={42,35,54,67,75,75}
+# s2={100,400,89,67,45,1000,670}
+# s3=s1.intersection(s2)
+# print(s3)
+
+# ***********Question 7 **********
+# Write a Python program to create a union of sets. 
+# s1={42,35,54,64,75,75}
+# s2={100,400,89,67,45,1000,670}
+# s3=s1.union(s2)
+# print(s3)
+
+# *************Question 8 ***********
+# Write a Python program to create set difference
+# s1={42,35,54,64,75,75}
+# s2={100,400,89,67,75,1000,670}
+# s3=s1.difference(s2)
+# print(s3)
+
+# *************Question 9 **********
+# Write a Python program to create a symmetric difference
+# s1={42,35,54,64,75,75}
+# s2={100,400,89,67,75,1000,670}
+# s3=s1.symmetric_difference(s2)
+# print(s3)
+
+# ************Question 10 ************
+# Write a Python program to issubset and issuperset.
+# s1={100,500,400,800}
+# s2={10,100,500,60,400,77,800,900}
+# s3=s1.issubset(s2)
+# s4=s1.issuperset(s2)
+# print(s3)
+# print(s4)
+
+# ***********Question 11 *************
+# Write a Python program to create a shallow copy of sets.
+# s1 = {10, 20, 30}
+# s2=s1.copy()
+# print(s2)
+
+# *********Question 12 *************
+# Write a Python program to clear a set.
+# s1 = {10, 20, 30}
+# s1.clear()
+# print(s1)
+
+# *********Question 13 **************
+# Write a Python program to use of frozensets.       
+# s = frozenset([10, 20, 30, 40])
+# print(s)
+# print(s.union({50, 60}))
+# print(s.intersection({20, 30, 50}))
+# print(s.difference({20}))
+
+# **********Question 14 **************
+# Write a Python program to find maximum and the minimum value in a set.
+# s1 = {10, 20, 30}
+# print(max(s1))
+# print(min(s1))
+
+# ***********Question 15 *************
+# Write a Python program to find the length of a set.
+s1 = {10, 20, 30}
+print(len(s1))
