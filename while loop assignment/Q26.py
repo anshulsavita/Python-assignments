7261b5f14bad4a5905de62c3a20c542e0dc183ee
Anshul savita <anshulsavita370@gmail.com>
2026-09-02 16:17:54 +0530

    added few questions of while loop assignment

--- while loop assignment/Q26.py
+++ while loop assignment/Q26.py
@@ -1,1 +1,60 @@
-# Question 26
+# *******Question 26***********
+# write a python program to check whether a number is prime number or not
+# n=int(input('Enter number:'))
+# if(n%2==0):
+#     print("Its is prime number..")
+# else:
+#     print("Not a prime number...")
+
+# *********Question 27 ***********
+# def armstrong(num):
+#     sum=0
+#     for i in n:
+#         m=1
+#         for j in range(1,len(n)+1):
+#             m=m*int(i)
+#         sum=sum+m
+#     if(sum==int(n)):
+#         return True
+#     else:
+#         return False
+# n=input("Enter number:")
+# k=armstrong(n)
+# print(k)
+
+# *************Question 28 ***********
+# def perfect(num):
+#     sum=0
+#     for i in range(1,num):
+#         if(num%i==0):
+#             sum=sum+i
+#     if(sum==num):
+#         return True
+#     else:
+#         return False
+# n=int(input("Enter number:"))
+# k=perfect(n)
+# print(k)
+
+# *************Question 29 ****************
+# def strong(num):
+#     sum=0
+#     for i in num:
+#         m=1
+#         for j in range(1,int(i)+1):
+#             m=m*j
+#         sum=sum+m
+#     if(sum==int(num)):
+#         return True
+#     else:
+#         return False
+
+# n=input("Enter number:")
+# k=strong(n)
+# print(k)
+
+# **************Question 30 *****************
+n=int(input('Enter number:'))
+for i in range(1,n+1):
+    if(i%2==0):
+        print(f"{i},",end='')
