fc9e422e135bf3d0a3ef7acc84e76fae7bf31511
Anshul savita <anshulsavita370@gmail.com>
2026-09-04 14:02:20 +0530

    added few more questions of while loop assignment

--- while loop assignment/Q26.py
+++ while loop assignment/Q26.py
@@ -149,5 +149,136 @@
 # ************Question 38 ***********
 # Write a python program to find two's complement of a binary number.
 # x='10011'
-# for i in range(len(x),0,-1):
-    
+# t=False
+# l=[]
+# for i in range(len(x)-1,-1,-1):
+#     if(x[i]!='1' and t==False):
+#         l.append(x[i])
+#     elif(x[i]=='1' and t==False):
+#         l.append(x[i])
+#         t=True
+#     elif(x[i]=='1' and t==True):
+#         l.append('0')
+#     elif(x[i]=='0'  and t==True):
+#         l.append('1')
+# x=''.join(l)
+# print(x[-1::-1])
+
+# ***********Question 39 **************
+# Write a python program to convert Binary to Octal number system.
+# x='11011'
+# if(len(x)%3!=0):
+#     t=True
+#     while(len(x)%3!=0):
+#         x=x.rjust(len(x)+1,'0')
+# i=0
+# l1=[]
+# while(i<len(x)):
+#     l2=[]
+#     for j in range(i,i+3):
+#         l2.append(x[j])
+#         i+=1
+#     if("".join(l2)=='000'):
+#         l1.append('0')
+#     elif("".join(l2)=='001'):
+#         l1.append('1')
+#     elif("".join(l2)=='010'):
+#             l1.append('2')
+#     elif("".join(l2)=='011'):
+#             l1.append('3')
+#     elif("".join(l2)=='100'):
+#             l1.append('4')
+#     elif("".join(l2)=='101'):
+#             l1.append('5')
+#     elif("".join(l2)=='110'):
+#             l1.append('6')
+#     elif("".join(l2)=='111'):
+#             l1.append('7')
+# print("".join(l1))
+
+# *************Question 40 ***************
+# write a python program to convert Binary to Decimal number system. 
+# l1=[]
+# t='00101101'
+# for i in range(0,len(t)):
+#     if(i==0):
+#         l1.append(1)
+#     else:
+#         l1.append((2*l1[i-1]))
+# l1.reverse()
+# total=0
+# for i in range(len(t)):
+#     if(t[i]=='1'):
+#         total=total+l1[i]
+# print(total)
+
+# ************Question 41 ******************
+# write a python program to convert Binary to Hexadecimal number system.
+# x='10110110'
+# if(len(x)%4!=0):
+#     t=True
+#     while(len(x)%4!=0):
+#         x=x.rjust(len(x)+1,'0')
+# i=0
+# l1=[]
+# while(i<len(x)):
+#     l2=[]
+#     for j in range(i,i+4):
+#         l2.append(x[j])
+#         i+=1
+#     if("".join(l2)=='0000'):
+#         l1.append('0')
+#     elif("".join(l2)=='0001'):
+#         l1.append('1')
+#     elif("".join(l2)=='0010'):
+#             l1.append('2')
+#     elif("".join(l2)=='0011'):
+#             l1.append('3')
+#     elif("".join(l2)=='0100'):
+#             l1.append('4')
+#     elif("".join(l2)=='0101'):
+#             l1.append('5')
+#     elif("".join(l2)=='0110'):
+#             l1.append('6')
+#     elif("".join(l2)=='0111'):
+#             l1.append('7')
+#     elif("".join(l2)=='1000'):
+#             l1.append('8')
+#     elif("".join(l2)=='1001'):
+#             l1.append('9')
+#     elif("".join(l2)=='1010'):
+#             l1.append('A')
+#     elif("".join(l2)=='1011'):
+#             l1.append('B')
+#     elif("".join(l2)=='1100'):
+#             l1.append('C')
+#     elif("".join(l2)=='1101'):
+#             l1.append('D')
+#     elif("".join(l2)=='1110'):
+#             l1.append('E')
+#     elif("".join(l2)=='1111'):
+#             l1.append('F')
+# print("".join(l1))
+
+# ***************Question 42 ***************
+# Write a python program to convert Octal to binary number system.
+x='57'
+t=''
+for j in x:
+    if(j=='0'):
+        t=t+'000'
+    elif(j=='1'):
+        t=t+'001'
+    elif(j=='2'):
+        t=t+'010'
+    elif(j=='3'):
+        t=t+'011'
+    elif(j=='4'):
+        t=t+'100'
+    elif(j=='5'):
+        t=t+'101'
+    elif(j=='6'):
+        t=t+'110'
+    elif(j=='7'):
+        t=t+'111'
+print(t)
